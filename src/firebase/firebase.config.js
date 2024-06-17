ff993d0fc69e920cd027ec3b7c2d7cb45a81894a
Meherab <mejerabxd@gmail.com>
2024-06-17 12:27:27 -0700

    added all

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -6,11 +6,11 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_APIKEY
-  authDomain: import.meta.env.VITE_AUTHDOMAIN
-  projectId: import.meta.env.VITE_PROJECTID
-  storageBucket: import.meta.env.VITE_STORAGEBUCKET
-  messagingSenderId: import.meta.env.VITE_MESSEGINGSenderID
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket:import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSEGINGSenderID,
   appId: import.meta.env.VITE_APPID
 };
 
